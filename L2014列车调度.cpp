c9a1b7b3a38910da09e878d92e607b4c17764abf
Candalk <2426170744@qq.com>
2017-08-29 23:55:07 +0800

    列车调度set方法

---
+++ L2014列车调度.cpp
@@ -0,0 +1,22 @@
+// L2014列车调度.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<set>
+using namespace std;
+int main()
+{
+	int N,tmp,num=0;
+	cin >> N;
+	set<int> s;
+	s.insert(0);
+	for (int i = 1; i <= N; i++) {
+		cin >> tmp;
+		if (*s.rbegin() > tmp)
+			s.erase(*(s.upper_bound(tmp)));
+		s.insert(tmp);
+	}
+	cout << s.size()-1;
+    return 0;
+}
